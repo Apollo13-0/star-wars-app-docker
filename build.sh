32e493bf93d499144d3169c883b90b34d1f99637
Apollo13 <calderon.ig08@estudiantec.cr>
2020-12-16 18:59:19 -0600

    add login

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ npm install
 echo "2/3 Starting the build of th react application..."
 npm run build
 
-echo "3/3 Packing the application into a dockert image..."
-docker build . -t star-wars-server
+echo "3/4 Packing the application into a dockert image..."
+docker build . -t apollo130/star-wars-server:latest
+
+echo "4/4 Pushing to dockert hub..."
+docker login -u apollo130 -p $dockerHubPassword
+docker push apollo130/star-wars-server:latest
 
 echo "Completed."
