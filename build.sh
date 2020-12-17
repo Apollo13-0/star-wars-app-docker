b519eeb2ad85e6ca64f293562749bf07499991b2
Apollo13 <calderon.ig08@estudiantec.cr>
2020-12-16 18:26:02 -0600

    install fix

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-echo "Starting the build of th react application..."
+echo "1/3 Starting the build of th react application..."
+npm install
+
+echo "2/3 Starting the build of th react application..."
 npm run build
 
-echo "Packing the application into a dockert image..."
+echo "3/3 Packing the application into a dockert image..."
 docker build . -t star-wars-server
 
 echo "Completed."
